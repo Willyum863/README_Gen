1f4770433a62f43c3e1f183e3d3c032907c71641
William Combee <william.combee@yahoo.com>
2023-03-28 13:28:42 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,64 @@ const path = require("path");
 const inquirer = require("inquirer")
 var generateMarkdown = require("../Develop/utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your project about? Provide a short description explaining the what, why, and how of your project."
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "what is requried to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use"
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators or and third-party assets"
+    },
+    {
+        type: "input",
+        name: "features",
+        message: "List your projects features"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "How can others contribute?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What tests can be run and how?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "what is your email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is your github username?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "what kind of license shoud your project have?",
+        choices: ["MIT","BSD","GLP","None"]
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
